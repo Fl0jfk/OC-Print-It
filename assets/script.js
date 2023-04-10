58021142d398fb1b2b9ce795d4285f408e3cc4b9
Florian Hacqueville-Mathi <florianhacqueville-mathi@iMac-de-Florian.local>
2023-04-10 11:42:30 +0200

    dots click

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+										/* VARIABLES */
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -15,29 +17,37 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
-
+]										
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const imageElement = document.querySelector(".banner-img");
 const taglineElement = document.querySelector(".tagline");
+const link = document.getElementsByClassName("dot");
+const dotsClick1 = document.querySelector(".dot_1");
+const dotsClick2 = document.querySelector(".dot_2");
+const dotsClick3 = document.querySelector(".dot_3");
+const dotsClick4 = document.querySelector(".dot_4");
 let timerSlider = setInterval(timer, 3000);
 let i = 0;
 
+											/* FONCTIONS */
+
+//Fonction reliant la class dot_selected à la slide en cours grâce au i
 function linkDot(){
-	const link = document.getElementsByClassName("dot");
 	for (let i = 0; i < slides.length; i++){
 		link[i].classList.remove("dot_selected");		
 	}
 	link[i].classList.add("dot_selected");
 }
 
+//Fonction montrant l'image et sa tagline selon la const slides ci-dessus, i reçoit sa valeur selon un timer et d'autres fonnctions 
 function showSlide() {
-  imageElement.src = `./assets/images/slideshow/${slides[i].image}`;
-  taglineElement.innerHTML = slides[i].tagLine;
-  linkDot();
+  	imageElement.src = `./assets/images/slideshow/${slides[i].image}`;
+  	taglineElement.innerHTML = slides[i].tagLine;
+  	linkDot();
 }
 
+//Fonction appelé dans un setInterval qui ajoute une valeur +1 à i
 function timer (){
 	i++;
 	if (i >= slides.length) {
@@ -46,6 +56,9 @@ function timer (){
 	  showSlide();
 }
 
+											/* METHODES */
+
+//Méthode déclenché par un click sur la class arrow_right, ajoute un +1 à i et retourne à 0 si elle dépasse le nombre de slide
 arrowRight.addEventListener("click", function(){
 	i++;
 	clearInterval(timerSlider)
@@ -56,12 +69,45 @@ arrowRight.addEventListener("click", function(){
 	timerSlider = setInterval(timer, 3000);
 });
 
+//Méthode déclenché par un click sur la class arrow_left, ajoute un +1 à i et retourne à 0 si elle dépasse le nombre de slide
 arrowLeft.addEventListener("click", function() {
 	i--;
-	clearInterval(timerSlider)
+	clearInterval(timerSlider);
   	if (i < 0) {
     	i = slides.length-1;
   	}
   	showSlide();
+	timerSlider = setInterval(timer, 3000);
+});
+
+//Méthode déclenché par un click sur la class dot_1, modifie la valeur de i
+dotsClick1.addEventListener("click", function() {
+	clearInterval(timerSlider);
+	i = 0;
+	showSlide();
+	timerSlider = setInterval(timer, 3000);
+});
+
+//Méthode déclenché par un click sur la class dot_2, modifie la valeur de i
+dotsClick2.addEventListener("click", function() {
+	clearInterval(timerSlider);
+	i = 1;
+	showSlide();
+	timerSlider = setInterval(timer, 3000);
+});
+
+//Méthode déclenché par un click sur la class dot_3, modifie la valeur de i
+dotsClick3.addEventListener("click", function() {
+	clearInterval(timerSlider);
+	i = 2;
+	showSlide();
+	timerSlider = setInterval(timer, 3000);
+});
+
+//Méthode déclenché par un click sur la class dot_4, modifie la valeur de i
+dotsClick4.addEventListener("click", function() {
+	clearInterval(timerSlider);
+	i = 3;
+	showSlide();
 	timerSlider = setInterval(timer, 3000);
 });
